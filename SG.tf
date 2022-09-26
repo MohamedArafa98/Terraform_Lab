f6920597fd3f24682527b78bf6f202dc73ec7c9f
Arafa <arafa@localhost.localdomain>
2022-09-26 21:47:33 +0200

    provisioner worked

--- SG.tf
+++ SG.tf
@@ -2,20 +2,20 @@ resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow ssh inbound traffic"
   vpc_id      = module.Network_Module.vpc_id
-#InBound
+  #InBound
   ingress {
-    description      = "ssh from VPC"
-    from_port        = 22
-    to_port          = 22
-    cidr_blocks      = ["0.0.0.0/0"]
-    protocol         = "tcp"
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
   }
   #OutBound
-   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
@@ -36,12 +36,12 @@ resource "aws_security_group" "allow_http_ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
-    ingress {
-    description      = "http on port 3000 from vpc cidr only"
-    from_port        = 3000
-    to_port          = 3000
-    protocol         = "tcp"
-    cidr_blocks      = [var.vpc_cidr]
+  ingress {
+    description = "http on port 3000 from vpc cidr only"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
     # ipv6_cidr_blocks = ["::/0"]
   }
 
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "region" {
-    type = string
-  
+  type = string
+
 }
 
 variable "vpc_cidr" {
